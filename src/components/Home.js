b6d0027d9b89f2387f0fba02c5b5a48e1ed426d7
Luke Oliff <luke@lukeoliff.com>
2018-06-11 12:55:01 +0100

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Button } from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
 const Home = () => (
   <React.Fragment>
